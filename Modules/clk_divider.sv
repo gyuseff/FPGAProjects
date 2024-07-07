bbe05f8c72d114c6a6fe35fdbb1a356c8a0fb8d2
Gabriel <gmyuseff@uc.cl>
2024-07-06 20:04:08 -0400

    Change to clk_divider

--- Modules/clk_divider.sv
+++ Modules/clk_divider.sv
@@ -2,9 +2,9 @@
 //It uses a counter for the division
 //You can change the parameters to decide on the estimated target frequency
 
-module clk_divider #(parameter TARGET_FREQ = 25_000_000, FPGA_CLK = 50_000_000) (input logic clk, rst, output logic clk_div);
+module clk_divider #(parameter TARGET_FREQ = 25_000_000, FPGA_FREQ = 50_000_000) (input logic clk, rst, output logic clk_div);
 
-	int target_counter = FPGA_CLK/(2*TARGET_FREQ); //This calculates the target value to the counter to toggle
+	int target_counter = FPGA_FREQ/(2*TARGET_FREQ); //This calculates the target value to the counter to toggle
  
 	logic [31:0] counter;
 	
