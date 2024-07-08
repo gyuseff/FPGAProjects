7abc463603fb75aa9e90b5be259964d8e843ff0d
Gabriel <gmyuseff@uc.cl>
2024-07-07 20:50:32 -0400

    Fixed concurrency issue

--- Modules/LED_7SEG_Controller.sv
+++ Modules/LED_7SEG_Controller.sv
@@ -19,14 +19,11 @@
 
 //`include "./clk_divider.sv"
 
-module LED_7SEG_Controller #(parameter REFRESH_FREQ=50, FPGA_FREQ=50_000_000) (input logic en_w, input logic [1:0] waddr, input logic [7:0] data, input logic clk, input logic rst,  output logic [3:0] LED_enables, output logic [6:0] LED_7SEG, output logic LED_dot);
+module LED_7SEG_Controller #(parameter REFRESH_FREQ=150, FPGA_FREQ=50_000_000) (input logic en_w, input logic [1:0] waddr, input logic [7:0] data, input logic clk, input logic rst,  output logic [3:0] LED_enables, output logic [6:0] LED_7SEG, output logic LED_dot);
 
     logic [7:0] LED_regs [4];
     logic refresh_clk;
     logic [1:0] raddr; //Auxiliary counter for the Display logic block. It indicates which display is read at the current refresh_cycle
-
-    logic [6:0] current_digit;
-    logic current_dot;
     
     clk_divider #(.TARGET_FREQ (REFRESH_FREQ), .FPGA_FREQ(FPGA_FREQ)) clk_div (.clk(clk), .rst(rst), .clk_div(refresh_clk));
 
@@ -37,32 +34,35 @@ module LED_7SEG_Controller #(parameter REFRESH_FREQ=50, FPGA_FREQ=50_000_000) (i
                     begin
                         LED_regs[i] <= 8'hff; //Initialized as 1 because display is active_low
                     end
-                    raddr <= 2'b00;
             end else begin
                 if (en_w) LED_regs[waddr] <= data;
             end
         end
 
-    always_ff@(posedge refresh_clk) 
+    always_ff@(posedge refresh_clk or negedge rst) 
         begin
-            if (raddr == 2'b11) raddr <= 2'b00;
-            else raddr <= raddr  + 1;
-
-            current_digit <= LED_regs[raddr][6:0];
-            current_dot <= LED_regs[raddr][7];
+				if (!rst) 
+				begin
+					raddr <= 2'b00;
+				end
+				else 
+				begin
+					if (raddr == 2'b11) raddr <= 2'b00;
+					else raddr <= raddr + 1;
+				end
         end
 
     always_comb 
         begin
+            LED_7SEG = rst ? LED_regs[raddr][6:0] : 7'b1111111;
+            LED_dot = rst ? LED_regs[raddr][7] : 1'b1;
 
-            LED_7SEG = current_digit;
-            LED_dot = current_dot;
-
-            //Each led should be turned on if at least one of its segments is 0 
-            LED_enables[0] = !(&LED_regs[0]);
-            LED_enables[1] = !(&LED_regs[1]);
-            LED_enables[2] = !(&LED_regs[2]);
-            LED_enables[3] = !(&LED_regs[3]);
+            //Each led should be turned on if at least one of its segments is 0 or his address is enabled (active low logic)
+            LED_enables[0] = (&LED_regs[0]) | !(raddr == 2'b00);
+            LED_enables[1] = (&LED_regs[1]) | !(raddr == 2'b01);
+            LED_enables[2] = (&LED_regs[2]) | !(raddr == 2'b10);
+            LED_enables[3] = (&LED_regs[3]) | !(raddr == 2'b11);
+				
 
         end
 endmodule
