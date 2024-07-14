197c3f09d8c66fb1d5ea2db9b46901827d7ce648
Gabriel <gmyuseff@uc.cl>
2024-07-14 13:45:20 -0400

    Added parametrized barrel shifter

--- FPGA_Prototyping_by_SV_Examples/Chapter3/3_12_1_MultiFunction_Barrel_shifter_tb.sv
+++ FPGA_Prototyping_by_SV_Examples/Chapter3/3_12_1_MultiFunction_Barrel_shifter_tb.sv
@@ -1,17 +1,22 @@
 `include "3_12_1_MultiFunction_Barrel_shifter.sv"
 
-module BarrelShifter_2_tb;
+module BarrelShifter_2_param_tb;
 
-reg [7:0] in;
-wire [2:0] amt;
+localparam DATA_WIDTH = 16;
+localparam SEL_WIDTH = $clog2(DATA_WIDTH);
+
+wire [DATA_WIDTH-1:0] in;
+wire [SEL_WIDTH-1:0] amt;
 reg lr;
-reg [7:0] out;
-reg [7:0] expected;
+reg [DATA_WIDTH-1:0] out;
+reg [DATA_WIDTH-1:0] expected;
 reg result;
 
 int i;
 
-BarrelShifter_2 DUT 
+
+
+BarrelShifter_2_param #(.DATA_WIDTH(DATA_WIDTH), .SEL_WIDTH(SEL_WIDTH)) DUT 
 (
     .in(in),
     .amt(amt),
@@ -21,27 +26,28 @@ BarrelShifter_2 DUT
 
 
 initial begin
-    $dumpfile("BarrelShifter_2.vcd");
-    $dumpvars(0, BarrelShifter_2_tb);
+    $dumpfile("BarrelShifter_2_param.vcd");
+    $dumpvars(0, BarrelShifter_2_param_tb);
 end
 
 assign result = (out == expected);
-assign amt = i[2:0];
+assign amt = i[SEL_WIDTH:0];
+assign in = $urandom();
 
 initial begin
-    #0; in <= 8'hed; lr <= 0; i <= 0;
+    #0; lr <= 0; i <= 0;
     #1; expected <= in;
     #10;
-    for(i = 0; i < 8; i++) begin
+    for(i = 0; i < 2**(SEL_WIDTH)-1; i++) begin
         #10;
-        expected <= {expected[0], expected[7:1]};
+        expected <= {expected[0], expected[DATA_WIDTH-1:1]};
     end
     #20; lr <= 1'b1; i <= 0;
     #1; expected <= in;
     #10;
-    for(i = 0; i < 8; i++) begin
+    for(i = 0; i < 2**(SEL_WIDTH)-1; i++) begin
         #10;
-        expected <= {expected[6:0], expected[7]};
+        expected <= {expected[DATA_WIDTH-2:0], expected[DATA_WIDTH-1]};
     end
     $stop;
 end
